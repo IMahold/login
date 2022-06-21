94085aa23314d01450fb836ec4a4f314f486bb31
Igor <machold.igor@gmail.com>
2022-06-21 15:31:06 +0200

    new adjustments

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,33 @@
 import React from "react";
 import Button from "@mui/material/Button";
-import { Container } from "@mui/system";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
-import { Grid, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
+import "./login.css";
 
 export default function Login() {
   return (
-    <div>
-      <Container>
-        <Grid sx={{ backgroundColor: "green" }}>
-          <Typography>Login to your Account</Typography>
-          <form action="">
-            <input type="text" placeholder="Enter Email" />
-            <input type="text" placeholder="Password" />
-            <VisibilityOffIcon />
-          </form>
-        </Grid>
-      </Container>
+    <div className="login-container">
+      <div className="login-form">
+        <form action="">
+          <Typography
+            variant="h6"
+            sx={{ marginBottom: "20px", color: "#464646", fontWeight: "600" }}
+          >
+            Login to your Account
+          </Typography>
+          <input type="text" placeholder="Enter Email *" />
+          <input type="text" placeholder="Password *" />
+          <p className="forgot">Forgot Password?</p>
+          <Button fullWidth variant="contained" sx={{ fontSize: "0.6rem" }}>
+            Login
+          </Button>
+          <p className="account">
+            Don't have an account? <span> Register now </span>
+          </p>
+
+          {/* <VisibilityOffIcon /> */}
+        </form>
+      </div>
     </div>
   );
 }
