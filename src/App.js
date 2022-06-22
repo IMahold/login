d30bfae1cffb8e0a7078fb52087957d4034e55f4
Igor <machold.igor@gmail.com>
2022-06-22 17:17:57 +0200

    image added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import React from "react";
 import Login from "./components/Login/Login";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Login />
     </div>
   );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./navbar.css";
+import imag from "../assets/Images/image1.png";
+import line1 from "../assets/Images/line1.png";
+import line2 from "../assets/Images/line2.png";
+import line3 from "../assets/Images/line3.png";
+
+export default function Navbar() {
+  return (
+    <div className="navbar-container">
+      <div className="nav-imag">
+        <img src={imag} alt="" />
+      </div>
+      <div className="nav-lines">
+        <img className="line1" src={line1} alt="" />
+        <img className="line2" src={line2} alt="" />
+        <img className="line3" src={line3} alt="" />
+      </div>
+    </div>
+  );
+}
